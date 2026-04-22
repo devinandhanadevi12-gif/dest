4e09046f91fd94ea4e1e2185634429317c06988b
devinandhanadevi12-gif <devinandhanadevi12@gmail.com>
2026-04-22 10:19:42 +0530

    test commit

--- oops/github_commands.py
+++ oops/github_commands.py
@@ -5,3 +5,5 @@
 # git remote add origin <repo-url>
 # git branch -M main
 # git push -u origin main
+ #git config --global user.name "devinandhanadevi12-gif"
+# git config --global user.email "devinandhanadevi12@gmail.com"
